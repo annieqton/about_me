5b9427a9a586eefc260689dbfc9dff0ff1a5bfcb
Annie Ton-Nu <annieqton@gmail.com>
2016-11-29 14:55:59 -0800

    renamed the varialbe for the user response so that it makes more sense

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 'use strict';
 
-// The questions in the guessing game must require a mix of yes/no answers, and user input must accept either y/n or yes/no responses, with either .toUpperCase() or .toLowerCase() used to validate the user input and accommodate users entering all-caps Y/N or YES/NO answers, too.
+var response1 = prompt('Does Annie live in Seattle?').toLowerCase();
 
-var question1 = prompt('Does Annie live in Seattle?').toLowerCase();
 
-/*if (condition is true) {
-  do something
-  maybe do something else
-  ext. until done
-} else {
-  do this other thing
-  and maybe yet another thing
-}*/
-
-if (question1 === 'yes' || question1 ==='y'){
+if (response1 === 'yes' || response1 ==='y'){
   document.write('Correct! You\'ve earned yourself a star.');
-  console.log('User answers question correctly');
-} else if(question1 === 'no' || question1 ==='n'){
+  console.log('User answers response correctly');
+} else if(response1 === 'no' || response1 ==='n'){
   document.write('Boo! You answered this question incorrectly!');
 } else{
-  document.write('Try to answer with a yes/no or y/n next time.');
+  document.write('Try to answer with a yes/y or no/n next time.');
 }
